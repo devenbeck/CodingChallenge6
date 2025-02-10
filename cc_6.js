30c89cf538b9cf8d5f59d19728e8614b726bb3ac
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-09 20:57:34 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,12 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {         //functio
 }; 
 console.log(calculateProfit(20,30,100));                 //should be (30-20)*100 =$1000
 console.log(calculateProfit(50,70,200));                 // = $4000
+
+//Task 2: Function Expression
+// Scenario: Sales Tax Computation
+const calculateSalesTax = function(amount, taxRate) {  //function for calculating sales tax
+    let salesTax = (amount * taxRate)
+    return `Sales Tax: $${salesTax}`;
+};
+console.log(calculateSalesTax(100, 0.07));   //$7 
+console.log(calculateSalesTax(500, 0.1));   // $50
