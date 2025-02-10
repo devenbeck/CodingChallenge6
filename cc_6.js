75e9bff298f4de44f55e4855199b5cf1a99a6170
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-09 21:08:31 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,22 @@ const calculateSalesTax = function(amount, taxRate) {  //function for calculatin
 };
 console.log(calculateSalesTax(100, 0.07));   //$7 
 console.log(calculateSalesTax(500, 0.1));   // $50
+
+//Task 3: Arrow Function
+// Scenario: Employee Bonus Calculation
+const calculateBonus = (salary, performanceRating) => {
+    let bonus;
+    if (performanceRating === "Excellent") {       // all the different options for bonuses based on performance
+        bonus = salary * 0.2;               //20% of salary
+    }
+    else if (performanceRating === "Good"){
+
+        bonus = salary * 0.1;          //10% of salary
+    }
+    else if (performanceRating === "Average"){
+        bonus = salary * 0.05;        //5% of salary
+    }
+    return `Bonus: $${bonus}`;
+}; 
+console.log(calculateBonus(5000, "Excellent"));    //printed result should be a $1000 bonus
+console.log(calculateBonus(7000, "Good"));         //should be $700 bonus
